2bf03227c15b6f11a72cbe25917f114b618fcdb6
snyder <snyderjosephd@yahoo.com>
2019-07-12 15:49:21 -0500

    added journal entry object and array to store the entry

---
+++ src/scripts/journal.js
@@ -0,0 +1,14 @@
+// Define the keys and values for a JavaScript object that represents a journal entry about what you learned today
+
+const journalEntry = {
+    date: "07/12/2019",
+    topic: "JavaScript DOM manipulation and Objects",
+    entry: "Today we learned how to manipulate html elements in the DOM and learned the basics of JavaScript objects and how to access their properties and values",
+    mood: "Good"
+}
+
+const journalEntries = [];
+
+journalEntries.push(journalEntry);
+console.log(journalEntries);
+
