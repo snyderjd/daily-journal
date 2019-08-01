094728964ccb018e40f15196b1a6849d48ad80ef
snyder <snyderjosephd@yahoo.com>
2019-08-01 15:56:44 -0500

    Added input validation to check for valid characters

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -15,33 +15,48 @@ data.getJournalEntries().then(parsedEntries => {
 
 const submitButton = document.querySelector('.submit');
 
+// Get references to the input containers
+const entryDate = document.querySelector('.date');
+const entryTopic = document.querySelector('.topic');
+const entryContent = document.querySelector('.entryContent');
+const entryMood = document.querySelector('.mood');
+
+// Save journal entry to the database when user clicks submit
 submitButton.addEventListener('click', (event) => {
 
     // Get references to the inputs and their values
-
-    const entryDate = document.querySelector('.date');
     const dateValue = entryDate.value;
-
-    const entryTopic = document.querySelector('.topic');
     const topicValue = entryTopic.value;
-
-    const entryContent = document.querySelector('.entryContent');
     const entryContentValue = entryContent.value;
-
-    const entryMood = document.querySelector('.mood');
     const moodValue = entryMood.value;
 
     // create a new Journal entry using the values in the form input fields
 
     const newEntry = createEntryObject(dateValue, topicValue, entryContentValue, moodValue);
 
-    saveJournalEntry(newEntry)
-        .then(data.getJournalEntries().then(parsedEntries => {
-            parsedEntries.forEach(entry => {
-                const HTMLRepresentation = entryComponent.createEntry(entry);
-                entriesDOM.addHTML(HTMLRepresentation);
-            });
+    const okayChars = 'abcdefghijklmnopqrstuvwxyz1234567890(){}:; !.';
+    const curseWords = ['fuck', 'shit', 'damn']
+
+    const entryChars = `${topicValue.toLowerCase()} ${entryContentValue.toLowerCase()}`;
+    const entryCharsArray = entryChars.split('');
+    const entryWords = entryChars.split(' ');
+
+    if (entryCharsArray.every(char => okayChars.includes(char)) 
+        && entryWords.every(word => !curseWords.includes(word))) {
+
+        saveJournalEntry(newEntry)
+            .then(data.getJournalEntries()
+            .then(parsedEntries => {
+                parsedEntries.forEach(entry => {
+                    const HTMLRepresentation = entryComponent.createEntry(entry);
+                    entriesDOM.addHTML(HTMLRepresentation);
+                });
         }));
+
+    } else {
+        window.alert(`Only the following characters may be entered: ${okayChars} Also, no curse words!`);
+    }  
+
 });
 
 
