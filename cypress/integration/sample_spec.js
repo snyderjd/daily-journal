b1be6f5a0723e90bda86ee65ec5e6529dc87d20e
Joe Snyder <snyderjd@Joes-MacBook-Pro.local>
2020-01-22 16:48:20 -0600

    Installed cypress and wrote first test

---
+++ cypress/integration/sample_spec.js
@@ -0,0 +1,15 @@
+describe('My First Test', function() {
+    it('Does not do much', function() {
+        expect(true).to.equal(true)
+    });
+});
+
+describt('My Second Test', function() {
+    it ('Does not do much', function() {
+        // Arrange - set up the application state (visit a page and query for an element)
+
+        // Act - take an action (interact with that element) 
+
+        // Assert - make an assertion (make an assertion about page content/changes to the app)
+    })
+})
