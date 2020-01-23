ae3acf2727f954d5f10bcad873e38a267432b230
Joe Snyder <snyderjd@Joes-MacBook-Pro.local>
2020-01-23 10:27:16 -0600

    Added second test for cypress example

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -1,15 +1,26 @@
+// Arrange - set up the application state (visit a page and query for an element)
+// Act - take an action (interact with that element)
+// Assert - make an assertion (make an assertion about page content/changes to the app)
+
 describe('My First Test', function() {
     it('Does not do much', function() {
         expect(true).to.equal(true)
     });
 });
 
-describt('My Second Test', function() {
-    it ('Does not do much', function() {
-        // Arrange - set up the application state (visit a page and query for an element)
+describe('My Second Test', function() {
+    it ('Clicks an element', function() {
+        cy.visit('https://example.cypress.io')
 
-        // Act - take an action (interact with that element) 
+        cy.pause()
 
-        // Assert - make an assertion (make an assertion about page content/changes to the app)
+        cy.contains('type').click()
+
+        cy.url()
+            .should('include', '/commands/actions')
+
+        cy.get('.action-email')
+            .type('fake@email.com')
+            .should('have.value', 'fake@email.com')
     })
 })
